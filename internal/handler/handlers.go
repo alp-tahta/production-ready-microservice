d2b5dca8df53aeb4af8dc09462d0c0e6ded69d4a
Alp Tahta <alp.tht48@gmail.com>
2025-03-14 04:02:15 +0300

    add error check for /health endpoint

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -10,10 +10,16 @@ type Handler struct {
 }
 
 func New(l *slog.Logger) *Handler {
-	return &Handler{l: l}
+	return &Handler{
+		l: l,
+	}
 }
 
 func (h *Handler) Health(w http.ResponseWriter, r *http.Request) {
-	h.l.Info("Health check log test") // TODO remove
-	w.Write([]byte("OK"))
+	_, err := w.Write([]byte("OK"))
+	if err != nil {
+		h.l.Error("Failed to write response", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
