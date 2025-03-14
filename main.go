41477f1e777ddfec72dd0fa6458e7084182511e2
Alp Tahta <alp.tht48@gmail.com>
2025-03-14 03:59:27 +0300

    refactor logger

--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-	"log/slog"
 	"net/http"
 	"os"
 
 	"github.com/alp-tahta/production-ready-microservice/internal/handler"
+	"github.com/alp-tahta/production-ready-microservice/internal/logger"
 	"github.com/alp-tahta/production-ready-microservice/internal/routes"
 	"github.com/alp-tahta/production-ready-microservice/internal/server"
 )
 
 func main() {
 	port := ":8080"
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	logger := logger.Init()
 	// logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	logger.Info("Starting HTTP server at", "port", port)
 
